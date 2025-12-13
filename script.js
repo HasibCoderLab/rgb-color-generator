7559ec6518d7c44a698f79d809fa0e6856e86b66
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-13 20:41:50 +0600

    solve function issue

--- script.js
+++ script.js
@@ -6,8 +6,22 @@
     const r = Math.floor(Math.random()*255);
     const g = Math.floor(Math.random()*256);
     const b = Math.floor(Math.random()*255);
-    return `rgb (${r} ,${g} , ${b} )`;    
+    return `rgb(${r} ,${g} , ${b} )`;    
  }
 
- console.log(generateColor());
+ //  Function for  update Color;
+
+ const updateColor = () =>{
+    const color = generateColor();
+    colorBox.style.backgroundColor = color;
+    colorCode.innerText = color
+ }
+
+//  Function for  show  Color code;
+copyButton.addEventListener("click" , () =>{
+    
+})
+updateColor()
+ generateColor()
+//  console.log(generateColor());
  
