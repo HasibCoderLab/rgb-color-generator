8f955a219a884f8ab0b12a94d3f01a5928e4f76e
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-13 20:29:20 +0600

    feat:successfuly generate rgb color in console

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
  const colorBox = document.getElementById("colorBox");
  const colorCode  = document.getElementById("colorCode") ;
  const copyButton  = document.getElementById("copyButton");
+
+ const generateColor = () =>{
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*255);
+    return `rgb (${r} ,${g} , ${b} )`;    
+ }
+
+ console.log(generateColor());
  
