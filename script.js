58710d7c6b9155e347b9144dd1bc25da4169b4f9
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-14 10:25:47 +0600

    create new function

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
- const colorBox = document.getElementById("colorBox");
- const colorCode  = document.getElementById("colorCode") ;
- const copyButton  = document.getElementById("copyButton");
+const colorBox = document.getElementById("colorBox");
+const colorCode = document.getElementById("colorCode");
+const copyButton = document.getElementById("copyButton");
 
- const generateColor = () =>{
-    const r = Math.floor(Math.random()*255);
-    const g = Math.floor(Math.random()*256);
-    const b = Math.floor(Math.random()*255);
-    return `rgb(${r} ,${g} , ${b} )`;    
- }
+const generateColor = () => {
+   const r = Math.floor(Math.random() * 255);
+   const g = Math.floor(Math.random() * 256);
+   const b = Math.floor(Math.random() * 255);
+   return `rgb(${r} ,${g} , ${b})`;
+}
 
- //  Function for  update Color;
+//  Function for  update Color;
 
- const updateColor = () =>{
-    const color = generateColor();
-    colorBox.style.backgroundColor = color;
-    colorCode.innerText = color
- }
+const updateColor = () => {
+   const color = generateColor();
+   colorBox.style.backgroundColor = color;
+   colorCode.innerText = color
+}
 
 //  Function for  show  Color code;
-copyButton.addEventListener("click" , () =>{
-    
+copyButton.addEventListener("click", () => {
+
 })
+const newColor = () => {
+
+   generateColor()
+}
 updateColor()
- generateColor()
+
 //  console.log(generateColor());
- 
