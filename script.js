0468dd8380b0c3d19ead0a96ebd098ec7a49f540
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-14 15:51:53 +0600

    comment optimization

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ======== Step 1 ====== atch all element
 const colorBox = document.getElementById("colorBox");
 const colorCode = document.getElementById("colorCode");
 const copyButton = document.getElementById("copyButton");
@@ -5,7 +6,7 @@ const generateColorBtn = document.getElementById("generateColorBtn");
 const popup = document.getElementById("successPopup")
 const btnIcon = document.querySelector("i");
 
-// generateColor RGB Color
+// ======== Step 2 ====== generateColor RGB Color
 const generateColor = () => {
    const r = Math.floor(Math.random()*255);
    const g = Math.floor(Math.random()*255);
@@ -13,14 +14,14 @@ const generateColor = () => {
    return `rgb(${r},${g},${b})`;
 }
 
-//  Update  Created Color
+// ======== Step 3 ====== Update  Created Color
 const updateColor = () => {
    const color = generateColor();
    colorBox.style.backgroundColor = color;
    colorCode.innerText = color;
 }
 
-//  copy color code and shoe popUp
+// ======== Step 4 ====== copy color code and shoe popUp
 const copyColorCode = () => {
    const text = colorCode.innerText;
    navigator.clipboard.writeText(text)
@@ -38,7 +39,8 @@ const copyColorCode = () => {
       }, 2000);
    });
 };
-//         ========= EventListeners======
+
+// ======== Step 5 ====== EventListeners======
 generateColorBtn.addEventListener("click", updateColor);
 copyButton.addEventListener("click", copyColorCode);
 document.addEventListener("DOMContentLoaded", updateColor);
