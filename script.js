9f15a54603f1a0ef1dc7e222d179ecfd2c7ad9df
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-14 12:28:11 +0600

    fixed :solve js some issue

--- script.js
+++ script.js
@@ -2,45 +2,37 @@ const colorBox = document.getElementById("colorBox");
 const colorCode = document.getElementById("colorCode");
 const copyButton = document.getElementById("copyButton");
 const generateColorBtn = document.getElementById("generateColorBtn");
+const popup = document.getElementById("successPopup")
 
+// generateColor RGB Color
 const generateColor = () => {
    const r = Math.floor(Math.random() * 255);
    const g = Math.floor(Math.random() * 256);
    const b = Math.floor(Math.random() * 255);
    return `rgb(${r} ,${g} , ${b})`;
 }
+//  Update  Created Color
 
-// copy Btn
-
-// function for copy btn
-const copyColorCode = () => navigator.clipboard.writeText(text);
-
-
-copyButton.addEventListener("click", copyColorCode)
-
-//  Function for  update Color;
-const updateColor = () => {
+const updateColor = () =>{
    const color = generateColor();
    colorBox.style.backgroundColor = color;
    colorCode.innerText = color;
 }
 
-//  function  for update UI with color in box
-const newColor = () => {
-   updateColor();
-}
-
-// Button click
-generateColorBtn.addEventListener("click", updateColor);
-
-document.addEventListener("DOMContentLoaded", updateColor)
-copyButton.forEach(copyBtn => {
-   copyBtn.addEventListener("click",() =>{
-      
-   })
-});
-
-
+//  copy color code and shoe popUp
+const copyColorCode  = () =>{
+   const text =  colorCode.innerText;
+   navigator.clipboard.writeText(text).then(() =>{
+      popup.classList.add("show");
+      setTimeout(() =>{
+popup.classList.remove("show");
+      },2000);
+   });
+};
+//         ========= EventListeners======
+generateColorBtn.addEventListener("click" , updateColor);
+copyButton.addEventListener("click" ,copyColorCode);
+document.addEventListener("DOMContentLoaded" ,updateColor);
 
 //  console.log(generateColor());
 // 
